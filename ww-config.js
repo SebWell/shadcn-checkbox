884736b34a769c019cd9d7c78ced092bb7e9158c
SebWell <sebastien.mignot@gmail.com>
2025-07-01 02:53:28 +0200

    FIX: Add complete Weweb configuration with all component options

--- ww-config.js
+++ ww-config.js
@@ -1,18 +1,69 @@
 export default {
     editor: {
-        label: 'checkbox',
+        label: 'Shadcn Checkbox',
         icon: 'check-square',
     },
     triggerEvents: [
         { name: 'click', label: 'On Click', event: { value: '' }, default: true },
+        { name: 'change', label: 'On Change', event: { value: '' } },
     ],
     properties: {
         content: {
-            label: 'Content',
+            label: 'Checkbox Settings',
             type: 'Object',
             section: 'settings',
             bindable: true,
-            defaultValue: {}
+            defaultValue: {
+                checked: false,
+                label: '',
+                disabled: false,
+                size: 'default',
+                customClasses: ''
+            },
+            options: {
+                item: {
+                    checked: {
+                        label: 'Checked',
+                        type: 'OnOff',
+                        section: 'settings',
+                        bindable: true,
+                        defaultValue: false
+                    },
+                    label: {
+                        label: 'Label',
+                        type: 'Text',
+                        section: 'settings',
+                        bindable: true,
+                        defaultValue: ''
+                    },
+                    disabled: {
+                        label: 'Disabled',
+                        type: 'OnOff',
+                        section: 'settings',
+                        defaultValue: false
+                    },
+                    size: {
+                        label: 'Size',
+                        type: 'TextSelect',
+                        section: 'settings',
+                        options: {
+                            choices: [
+                                { value: 'sm', label: 'Small' },
+                                { value: 'default', label: 'Default' },
+                                { value: 'lg', label: 'Large' }
+                            ]
+                        },
+                        defaultValue: 'default'
+                    },
+                    customClasses: {
+                        label: 'Custom Classes',
+                        type: 'Text',
+                        section: 'style',
+                        bindable: true,
+                        defaultValue: ''
+                    }
+                }
+            }
         }
     }
 };
